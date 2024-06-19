54236767654620d4c916e0cff65696419fb9dd27
Ning Yang <yangn0@qq.com>
2024-06-19 11:25:14 +0800

    update

--- buildbsp.sh
+++ buildbsp.sh
@@ -2,8 +2,19 @@
 export rtems_src=rtems
 # export rtems_src=rtems
 
-cd /home/yangn0/devel/RTEMS_devel/src/$rtems_src
-# ./waf distclean
-./waf configure --prefix=$HOME/devel/RTEMS_devel/rtems/6
+cd /home/yangn0/RTEMS_devel/src/$rtems_src
+./waf distclean
+./waf configure --prefix=$HOME/RTEMS_devel/rtems/6
 ./waf
 ./waf install
+
+export bsp=xilinx_versal_qemu
+app=/home/yangn0/RTEMS_devel/src/rtems/build/aarch64/$bsp/testsuites/samples/hello.exe
+# app=/home/yangn0/RTEMS_devel/src/rtems/build/aarch64/$bsp/testsuites/sptests/spconsole01.exe
+# cp $app /mnt/c/Users/79230/Desktop/test.exe
+# qemu-system-aarch64.exe -no-reboot -nographic -serial mon:stdio -machine virt,gic-version=3 -cpu cortex-a72 -m 4096 -d trace:pl011_baudrate_change -kernel $app
+
+# app=/home/yangn0/RTEMS_devel/src/rtems/build/aarch64/a53_lp64_qemu/testsuites/sptests/spconsole01.exe
+# qemu-system-aarch64.exe -no-reboot -nographic -serial mon:stdio  -machine virt,gic-version=3 -cpu cortex-a53 -m 4096 -d trace:pl011_baudrate_change -kernel $app
+
+# qemu-system-aarch64.exe -no-reboot -nographic -serial mon:stdio -machine xlnx-versal-virt -m 4096 -d trace:pl011_baudrate_change -kernel $app
--- buildlibbsd.sh
+++ buildlibbsd.sh
@@ -3,10 +3,11 @@
 #aarch64/raspberrypi4b
 #aarch64/xilinx_versal_qemu
 #xilinx_zynqmp_lp64_qemu
-cd /home/yangn0/devel/rtems-aarch64/src/rtems-libbsd
-./waf distclean
-./waf configure --prefix=$HOME/devel/rtems-aarch64/rtems/6 \
-      --rtems-bsps=aarch64/raspberrypi4b \
+#arm/xilinx_zynq_a9_qemu
+cd /home/yangn0/RTEMS_devel/src/rtems-libbsd
+# ./waf distclean
+./waf configure --prefix=$HOME/RTEMS_devel/rtems/6 \
+      --rtems-bsps=arm/xilinx_zynq_a9_qemu \
       --buildset=buildset/default.ini
 ./waf
 ./waf install
