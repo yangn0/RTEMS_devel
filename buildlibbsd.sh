d2a5759375cec8dcab12c5dd739ca136a914c510
Ning Yang <yangn0@qq.com>
2025-06-21 13:41:40 +0800

    buildlibbsd.sh: 增加配置变量 提高灵活性

--- buildlibbsd.sh
+++ buildlibbsd.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
-export rtems_version=6
+rtems_version=6
+
+qemu_test=0
+rtems_libbsd_path=$HOME/RTEMS_devel/src/rtems-libbsd
+rtems_prefix=$HOME/RTEMS_devel/rtems/$rtems_version
 export PATH=$HOME/RTEMS_devel/rtems/$rtems_version/bin:$PATH
-
-export arch=aarch64
-export bsp=zynqmp_qemu
-# export bsp=xilinx_zynq_a9_qemu
-
-# export arch=aarch64
-# export bsp=raspberrypi4b
+arch=aarch64
+bsp=zynqmp_qemu
+# bsp=xilinx_zynq_a9_qemu
+# arch=aarch64
+# bsp=raspberrypi4b
 #xilinx_versal_qemu
 #xilinx_zynqmp_lp64_qemu xilinx_zynq_a9_qemu
 
+buildset=everything
+# test_name=media01
+# test_name=ipsec01
+# test_name=crypto01
+# test_name=unix01
+# test_name=syscalls01
+test_name=ttcpshell01
 
-export buildset=everything
-# export test_name=media01
-export test_name=ipsec01
-# export test_name=crypto01
-
-cd ~/RTEMS_devel/src/rtems-libbsd
+cd $rtems_libbsd_path
 # ./waf uninstall
 ./waf distclean
-./waf configure --prefix=$HOME/RTEMS_devel/rtems/$rtems_version \
+./waf configure --prefix=$rtems_prefix \
       --rtems-bsps=$arch/$bsp \
-      --buildset=buildset/$buildset.ini
+      --buildset=buildset/$buildset.ini \
+      # --optimization 1
       # --enable-auto-regen
 ./waf
 ./waf install
 
-if [ "$bsp" = xilinx_zynq_a9_qemu ]; then
-
+if [ "$bsp" = xilinx_zynq_a9_qemu -a $qemu_test == 1 ]; then
 # sudo ip tuntap add qtap mode tap user $(whoami)
 # sudo ip link set dev qtap up
 # sudo ip addr add 169.254.1.1/16 dev qtap
@@ -40,8 +44,7 @@ qemu-system-arm  -S -s -serial null -serial mon:stdio -nographic \
   -kernel build/$arm-rtems6-xilinx_zynq_a9_qemu-$buildset/$test_name.exe
 fi
 
-if [ "$bsp" = raspberrypi4b ]; then
-
+if [ "$bsp" = raspberrypi4b -a $qemu_test == 1 ]; then
 cd ~/RTEMS_devel
 # $arch-rtems6-objcopy -O binary $HOME/RTEMS_devel/src/rtems-libbsd/build/aarch64-rtems6-raspberrypi4b-$buildset/media01.exe kernel8.img
 # $arch-rtems6-objcopy -O binary $HOME/RTEMS_devel/src/rtems-libbsd/build/aarch64-rtems6-raspberrypi4b-$buildset/telnetd01.exe kernel8.img
@@ -50,19 +53,16 @@ $arch-rtems6-objcopy -O binary $HOME/RTEMS_devel/src/rtems-libbsd/build/aarch64-
 # $arch-rtems6-objcopy -O binary $HOME/RTEMS_devel/src/rtems-libbsd/build/aarch64-rtems6-raspberrypi4b-$buildset/ping01.exe kernel8.img
 # $arch-rtems6-objcopy -O binary $HOME/RTEMS_devel/src/rtems-libbsd/build/aarch64-rtems6-raspberrypi4b-$buildset/init01.exe kernel8.img
 # $arch-rtems6-objcopy -O binary $HOME/RTEMS_devel/src/rtems-libbsd/build/aarch64-rtems6-raspberrypi4b-$buildset/ipsec01.exe kernel8.img
-
 # $arch-rtems6-objcopy -O binary $HOME/RTEMS_devel/rtems/6/bin/ttcpshell01.exe kernel8.img
 cp kernel8.img /mnt/c/Users/79230/Desktop/tftp/
-
 # qemu-system-aarch64 -M raspi4b -serial mon:stdio -nographic -kernel /mnt/c/Users/79230/Desktop/tftp/kernel8.img
-
 fi
 
-if [ "$bsp" = zynqmp_qemu ]; then
+if [ "$bsp" = zynqmp_qemu -a $qemu_test == 1 ]; then
 # sudo ip tuntap add qtap mode tap user $(whoami)
 # sudo ip link set dev qtap up
 # sudo ip addr add 169.254.1.1/16 dev qtap
-qemu-system-aarch64 -no-reboot -nographic  \
+qemu-system-aarch64  -no-reboot -nographic  \
     -serial mon:stdio -machine xlnx-zcu102 -m 4096 \
     -net nic,model=cadence_gem \
     -net tap,ifname=qtap,script=no,downscript=no \
